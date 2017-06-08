1ec30887fe9db113a22e4333259502d6a0cd9aa1
Nurul Izza <nism576@aucklanduni.ac.nz>
2017-06-08 12:39:18 +1200

    Added documentation on class and function

--- source/drhtaxes/corporate.py
+++ source/drhtaxes/corporate.py
@@ -1,6 +1,12 @@
 class COR(object):
+        """
+        Tax calculation for the Democratic Republic of Hughland when filing as a corporation
+        """
 
 	def corp(self,income,incurred_loss):
+        """
+        Input will be the income and incurred losses
+        """
 
 		if income < 80000:
 			tax = (income*0.2) #If income is below $80,000, 20% applied to the total income
