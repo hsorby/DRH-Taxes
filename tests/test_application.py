0a4d96ea78a3247e8cdc5748f715ab86030e2b26
Toby Jackson <tjac799@aucklanduni.ac.nz>
2017-06-08 13:46:29 +1200

    test that applications module exists

--- tests/test_application.py
+++ tests/test_application.py
@@ -3,13 +3,12 @@ import inspect
 
 from drhtaxes import application
 
-
 class ApplicationTests(unittest.TestCase):
     
     def test_application(self):
         self.assertTrue(inspect.ismodule(application))
 
-
+    
 if __name__ == '__main__':
     unittest.main()
 
