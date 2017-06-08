2c6b6fce8847dc419784aef599c10cdd36e9dbd8
Nurul Izza <nism576@aucklanduni.ac.nz>
2017-06-08 12:19:15 +1200

    Deleted the print statement and added documentation

--- source/drhtaxes/corporate.py
+++ source/drhtaxes/corporate.py
@@ -3,9 +3,9 @@ class COR(object):
 	def corp(self,income,incurred_loss):
 
 		if income < 80000:
-			tax = (income*0.2)
+			tax = (income*0.2) #If income is below $80,000, 20% applied to the total income
 	
 		elif income >= 80000:
-			tax = (income - incurred_loses)*0.2
+			tax = (income - incurred_loses)*0.2 #If income is above $80,000, then incurred losses can be subtracted from income
 		return tax
-		print (tax)
+		
