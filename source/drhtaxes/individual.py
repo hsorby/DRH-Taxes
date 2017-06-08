26a41bdc5d20d60f965dd94671dd1f9798f55015
arcisad <a.rastar@auckland.ac.nz>
2017-06-08 12:32:28 +1200

    Added comments for the functions in the class

--- source/drhtaxes/individual.py
+++ source/drhtaxes/individual.py
@@ -1,16 +1,19 @@
 class Tax(object):
     """
-    This class takes income and loss as the input and claculates the individual tax based on tier income and loss.
+    This class takes income and loss as the input and claculates the individual tax.
     """
     def calc_tax(self,income):
+        """
+        The function tries to calculate the income tax for every individual depending on the level of income.
+        """
         try:
             if income < 10000:
-                return income
+                return 0
             elif 10000 < income < 30000:
-                return income - (income*0.17)
+                return income*0.17
             elif 30000 < income < 70000:
-                return income - (income*0.28)
+                return income*0.28
             else:
-                return income - (income*0.35)
+                return income*0.35
         except ValueError:
             pass
