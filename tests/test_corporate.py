0835e3e37137121d4195e0d01addfd1df438d758
Nurul Izza <nism576@aucklanduni.ac.nz>
2017-06-09 09:25:00 +1200

    Deleted the print statement and added few more test

--- tests/test_corporate.py
+++ tests/test_corporate.py
@@ -15,11 +15,11 @@ class ApplicationTests(unittest.TestCase):
         Testing function_corp
         """
         tax = COR()
-        print(tax.corp(79000, 0))
         self.assertEqual(15800, tax.corp(79000, 0))
-        #self.assertTrue(tax.corp(79000,0) == 15800)
         self.assertEqual(16980, tax.corp(85000,100))
-
+        self.assertEqual(400, tax.corp(2000,0))
+        self.assertEqual(19000, tax.corp(100000,5000))
+        self.assertEqual(8000, tax.corp(40000,0))
 
 if __name__ == '__main__':
     unittest.main()
